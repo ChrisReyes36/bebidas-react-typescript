c80b9c7e9a11fd3a08fd2297038794aee57a6c53
Christopher Reyes <christopherreyes456@gmail.com>
2026-06-11 22:27:54 -0600

    feature(app): modelo dinámico

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -2,8 +2,10 @@ import { streamText } from "ai";
 import { openRouter } from "../utils/ai";
 
 export async function generateRecipe(prompt: string) {
+  const model = import.meta.env.VITE_MODEL_AI as string;
+
   const result = streamText({
-    model: openRouter("nex-agi/nex-n2-pro:free"),
+    model: openRouter(model),
     prompt,
     system:
       "Eres un bartender que tiene 20 años de experiencia y le sirvió una bebida James Bond",
